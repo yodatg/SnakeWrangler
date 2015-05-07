06364d3ec7142d736049bf44398f73e37a70b6ed
Tom <tomgrant00@gmail.com>
2015-05-07 11:22:19 +0100

    Initial Commit

---
+++ SnakeWranglerCheckIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnakeWranglerCheckIn
+//
+//  Created by Thomas Grant on 07/05/2015.
+//  Copyright (c) 2015 Thomas Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
